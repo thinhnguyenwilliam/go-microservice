4ee0ca7def4fdd21e714b7b7fcc8b82d0c939c16
ThinhNguyen <thinhproee@gmail.com>
2025-04-09 16:46:09 +0700

    error handler fixing

--- pkg/response/httpStatusCode.go
+++ pkg/response/httpStatusCode.go
@@ -1,1 +1,11 @@
 package response
+
+const (
+	ErrorCodeSuccess      = 20001 // Success
+	ErrorCodeParamInvalid = 20003 // Email is invalid
+)
+
+var msg = map[int]string{
+	ErrorCodeSuccess:      "Success",
+	ErrorCodeParamInvalid: "Email is invalid",
+}
